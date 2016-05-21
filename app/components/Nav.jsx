c35fde787daf3f3fbb58d0ce8d000d20ce88a5b4
DevEMCN <devemcn@gmail.com>
2016-05-21 14:43:27 +0100

    initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Pomodoro Clock and Stopwatch</li>
+            <li>
+              <IndexLink to="/" activeClassName="active">Pomodoro Clock</IndexLink>
+            </li>
+            <li>
+              <Link to="" activeClassName="active">Stopwatch</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Created by <a href="https://github.com/DevEMCN" target="_blank">DevEMCN</a></li>
+          </ul>
+        </div>
+      </div>
+    );
+};
+
+module.exports = Nav;
