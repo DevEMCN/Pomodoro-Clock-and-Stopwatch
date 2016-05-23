cac39ab98e27bd4f0c28e88d3ca804c2c16940a1
DevEMCN <devemcn@gmail.com>
2016-05-23 15:48:28 +0100

    add countdown functionality

--- app/components/PomodoroClock.jsx
+++ app/components/PomodoroClock.jsx
@@ -6,11 +6,37 @@ var PomodoroClock = React.createClass(
 {
   getInitialState: function()
   {
-    return {count: 0};
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  startTimer: function()
+  {
+    this.timer = setInterval( () => {
+      var newCount = this.state.count -1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
+  },
+  componentDidUpdate: function(prevProps, prevState)
+  {
+    if(this.state.countdownStatus !== prevState.countdownStatus)
+    {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        default:
+
+      }
+    }
   },
   handleSetCountdown: function(seconds){
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started'
     });
   },
   render: function()
